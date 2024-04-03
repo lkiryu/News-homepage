a3ccd857b8911081865b1735e01fe9f6bd27ca9c
Kiryu <13matheusilva@gmail.com>
2024-04-03 12:28:48 -0300

    responsive add

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,13 +4,19 @@ import logo from '../../images/logo.svg'
 import './header.css'
 
 const Header = () => {
-    return(
+    return (
         <header className="header">
             <a href="/">
                 <img src={logo} alt="logo" className="logo"></img>
             </a>
 
             <nav>
+                <div className="mobile-menu">
+                    <div className="line1"></div>
+                    <div className="line2"></div>
+                    <div className="line3"></div>
+                </div>
+
                 <ul className="nav-list">
                     <li className="nav-link"><a href="/">Home</a></li>
                     <li className="nav-link"><a href="/">New</a></li>
